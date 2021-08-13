56d2d40c1d4403a5c981828e94cf5db929174570
Nicole Deshong <nicoleanjalii@gmail.com>
2021-08-12 19:19:47 -0700

    adds images into thumbnail

--- data/games-year.js
+++ data/games-year.js
@@ -7,7 +7,7 @@
 				"Company":"Sega",
 				"Year":1991,
 				"Rating":"E",
-				"Image":"images/sonic-the-hedgehog.jpg"
+				"Image":"sonic-the-hedgehog.jpg"
 			  },
               {
 				"Title":"Super Mario Kart",
